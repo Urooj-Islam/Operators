0dd374ee903491248315724a09305c20c87b98ac
UroojIslamKhan <121480546+Urooj-Islam@users.noreply.github.com>
2022-12-31 12:04:24 +0500

    Add files via upload

---
+++ Arithmatic operators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=2, b=3;
+	cout<<"a+b= "<<a+b<<endl;
+	 cout << "The value of a - b is " << a - b << endl;
+    cout << "The value of a * b is " << a * b << endl;
+    cout << "The value of a / b is " << a / b << endl;
+    cout << "The value of a % b is " << a % b << endl;
+    cout << "The value of a++ is " << a++ << endl;
+    cout << "The value of a-- is " << a-- << endl;
+    cout << "The value of ++a is " << ++a << endl;
+    cout << "The value of --a is " << --a << endl;
+    return 0;
+}
