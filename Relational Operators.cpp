636e8a32ab02d0e04f9d9f095d704b4ed3758b2e
UroojIslamKhan <121480546+Urooj-Islam@users.noreply.github.com>
2023-01-03 17:00:50 +0500

    Add files via upload

---
+++ Relational Operators.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=5, b=8;
+	cout<<a<<" is greater than "<<b<<"? "<<(a>b)<<endl;
+	cout<<a<<" is less than"<<b<<"? "<<(a<b)<<endl;
+	cout<<a<<" is greater than or equal to"<<b<<"? "<<(a>=b)<<endl;
+	cout<<a<<" is less than or equal to "<<b<<"? "<<(a<=b)<<endl;
+	cout<<a<<" is equal to "<<b<<"? "<<(a==b)<<endl;
+	cout<<a<<"is not equal to "<<b<<"? "<<(a!=b)<<endl;
+}
